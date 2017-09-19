94de0ecc44f992792d512a07ddb4af8eceddcc77
Matthew LePine <lepinem2002@gmail.com>
2017-09-19 19:28:37 -0400

    kinda_working

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,9 @@
 import React, {Component} from 'react';
 import '../styles/App.css';
 import Jumbotron from './Jumbotron'
-import getVehicles from '../services'
+
 
 class App extends Component {
-  // PROPS AND STATE
-  // Set props and state below.
-  // You should set state for vehicles (empty array), value (empty string), pilot (empty) string.
-  // Enter your code below:
 
   constructor(props) {
     super(props);
@@ -18,23 +14,7 @@ class App extends Component {
       pilots: [],
       vehicles: []
     }
-
-  // FORM: HANDLE INPUT CHANGES
-  // handleNameChange below:
-  // See form lesson for details.
-  // Enter your code below:
-
-    // this.handlePilotTextChange = this.handlePilotTextChange.bind(this);
-    // // this.handleValueChange = this.handleValueChange.bind(this);
-    // this.handleFormSubmit = this.handleFormSubmit.bind(this);
   }
-
-  //  FORM: SUBMIT METHOD
-  // handleSubmit below:
-  // See form lesson for details.
-  // Once the form is sumbited, two things need to happen: set the state of pilot to the input value.
-  // Then, set the value of the input back to an empty string.
-  // Enter your code below:
 
   handlePilotTextChange(submit){
     this.setState({
@@ -64,48 +44,17 @@ class App extends Component {
     })
   }
 
-  // LIFECYCLE
-  // Which lifecycle is best for fetching data?
-  // Inside this lifecycle, you will fetch the vehicles from here: https://swapi.co/api/vehicles/
-  // Once you have fetched that data, set the state of vehicles to the fetched data.
-  // In your response look for 'results'. It should return this array.
-  // You will want to use this array when you set the state of 'vehicles'. You will need this data in your render.
-  // Enter your code below:
-
-  // fetchVehicles = (showVal) => {
-  //    getVehicles(showVal).then((response) => {
-  //       this.setState({ vehicles: response })
-  //    })
-  // }
-  // componentDidMount(){
-  //    this.fetchVehicles()
-  // }
-
-  //
   componentWillMount() {
     fetch('https://swapi.co/api/vehicles/')
     .then(r => r.json() )
     .then((json) => {
-      console.log(json)
       this.setState({vehicles: json.results})
     })
   }
-  //
-  // RENDER
-  // Before you can map over the data you've fetched, you will first need to store that 'state' in a variable.
-  // // Map over the data.
-  // // Don't forget to set the 'key'. In this case, use the vehicle name.
-  // You will need the following values: name, model, manufacturer, class, passengers, crew, length, max speed, and cargo capacity.
-  // Rendering: create a 'card' for each of the vehicles. consult the Bootstrap 4 docs for details.
-  // Enter your code below:
 
   render() {
-    let vehicles = this.state.vehicles
-    /*
-    Store vehicles state in a variable.
-    Map over this variable to access the values needed to render.
-    */
-    // })
+    let vehicles = (this.state.vehicles).map((vehicle) => {
+
     return (
       <div className="App">
         <Jumbotron />
@@ -124,20 +73,39 @@ class App extends Component {
               {this.state.pilots.map((pilot) => {
                 return(
                   <div>
-                  <h2>{pilot.pilot}</h2>
-                </div>
-              )
+                    <h2>{pilot.pilot}</h2>
+                  </div>
+                )
               })}
             </div>
           </div>
         </div>
-        {/*
-        The App component needs the following:
-         jumbotron section, form section, vehicle cards section.
-         Your form will also need a header in which you will pass the state of the form upon submit.
-         */}
+        <div className="card" >
+          <div className="card-block">
+            <h4 className="card-title">Name: {vehicle.name}</h4>
+            <h6 className="card-subtitle mb-2 text-muted">Model: {vehicle.model}</h6>
+            <ul class="list-group list-group-flush">
+              <h6 class="list-group-item-heading">Specs</h6>
+                <li class="list-group-item">Manufacturer: {vehicle.manufacturer}</li>
+                <li class="list-group-item">Class: {vehicle.vehicle_class}</li>
+                <li class="list-group-item">Passengers: {vehicle.passengers}</li>
+                <li class="list-group-item">Crew: {vehicle.crew}</li>
+                <li class="list-group-item">Length: {vehicle.length}</li>
+                <li class="list-group-item">Max Speed: {vehicle.max_atmosphering_speed}</li>
+                <li class="list-group-item">Cargo: {vehicle.cargo_capacity}</li>
+            </ul>
+          </div>
+        </div>
       </div>
-    );
+    )
+    })
+    return (
+      <div className="quake-list">
+        <div className="row">
+          {vehicles}
+        </div>
+      </div>
+    )
   }
 }
 
